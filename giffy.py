669935b0453956e6d6b7a66b0341b80198be5326
moquito64 <blanchette.sebastian@gmail.com>
2022-11-10 15:25:57 -1000

    Update giffy.py
    Added adjustments for scale

--- giffy.py
+++ giffy.py
@@ -1,21 +1,27 @@
-
+""" Uses tkinter to create a frame and play an asset file """
 from tkinter import *
+from PIL import ImageTk, Image
 #import time
 #import os
-root = Tk()
+root = Tk(className = '- Digital Monster -')
+root.geometry("240x320")
 
-frameCnt = 4
-frames = [PhotoImage(file='assets/greymon.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+FRAMECNT = 4
+GM_FILE = 'assets/greymon-scaled.gif'
+
+frames = [PhotoImage(file=GM_FILE,format = f'gif -index {i}') for i in range(FRAMECNT)]
+
 
 def update(ind):
-
+    """ Keep the frame up to date """
     frame = frames[ind]
     ind += 1
-    if ind == frameCnt:
+    if ind == FRAMECNT:
         ind = 0
     label.configure(image=frame)
     root.after(100, update, ind)
 label = Label(root)
 label.pack()
+label.config(bg="#24201b")
 root.after(0, update, 0)
 root.mainloop()
